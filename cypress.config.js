9f4411018bcc4b22597d49ffe09af5e2001faa32
Yuri Mielli <yumielllli@gmail.com>
2026-07-22 19:09:24 +0200

    atualizando cypress no projeto

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3000/?lang=pt-BR',
+
+    setupNodeEvents(on, config) {
+      return config
+    }
+  }
+})
